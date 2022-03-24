b968add20a732ba17807a8dbd68ddd241820cfe1
Villalba Agustín <villalba.agus.99@gmail.com>
2022-03-24 09:51:31 -0300

    Cap_4sinHistorioCofla

---
+++ Cap_4/codigo.js
@@ -0,0 +1,31 @@
+let cadena = "bc ";
+let cadena2 = " Hola ";
+let cadena3 = 1235;
+
+/* resultado = cadena.concat(" Hola ", cadena2); */
+/* resultado = cadena.startsWith(cadena2); */
+/* resultado = cadena.endsWith(cadena2); */
+/* resultado = cadena.includes(cadena2); */
+/* resultado = cadena.indexOf(cadena2); */
+/* resultado = cadena.lastIndexOf(cadena2); */
+/* resultado = cadena.padStart(4,"a"); */
+/* resultado = cadena.padEnd(4,"de"); */
+/* resultado = cadena.repeat(3); */
+/* resultado = cadena2.split(" "); */
+/* resultado = cadena2.substring(0,4); */
+/* resultado = cadena2.toLowerCase(); */
+/* resultado = cadena2.toUpperCase(); */
+/* resultado = cadena3.toString(); */
+/* resultado = cadena2.toString(); */
+/* resultado = cadena2.trimEnd(); */
+/* resultado = cadena2.trimStart(); */
+
+
+ 
+
+
+
+/* document.write(resultado.length); */
+/* document.write(resultado.trim()); */
+document.write(resultado);
+document.write(resultado.length);
